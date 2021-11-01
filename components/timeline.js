03760a711d82e6aec15ed45f8e6a2d4762531b3f
Rowan <rowanfuchs@gmail.com>
2021-11-01 19:34:49 +0100

    changes

--- components/timeline.js
+++ components/timeline.js
@@ -2,23 +2,23 @@ import React from "react";
 import {connect} from "react-redux";
 import Portfolio from '../public/portfolio';
 
-const Timeline = (state) => {
+const Timeline = () => {
 
     return (
         <div className="antialiased text-gray-500 dark:bg-gray-800">
             <div className="container relative flex flex-col px-6 mx-auto">
-                <div className="absolute inset-0 z-0 w-2 h-full bg-gray-200 shadow-md left-17 md:mx-auto md:right-0 md:left-0"/>
+                <div className="absolute inset-0 z-0 w-2 h-full bg-gray-200 shadow-md left-17 md:mx-auto md:right-0 md:left-0 dark:bg-gray-900"/>
                 {Portfolio && Portfolio.map((project, index) => (
                     <div className="relative z-10 py-8" key={index}>
-                        <img src={project.image} alt={project.title} className="timeline-img"/>
+                        <img src={project.image} alt={project.title} className="timeline-img absolute"/>
                         <div
                             className={`${index % 2 === 0 ? "timeline-container" : "timeline-container timeline-container-left"}`}>
                             <div
                                 className={`${index % 2 === 0 ? "timeline-pointer" : "timeline-pointer timeline-pointer-left"}`}
                                 aria-hidden="true"/>
-                            <div className="p-3 text-left text-sm bg-gray-200 rounded-md shadow-md">
+                            <div className="p-3 text-left text-sm bg-gray-200 rounded-md shadow-md dark:bg-gray-900">
                                 {project.date &&
-                                    <p className="text-l font-bold text-purple-400 py-1">{project.date}</p>
+                                    <p className="text-l font-bold text-purple-400 py-1 dark:text-purple-800">{project.date}</p>
                                 }
                                 <p className="text-xl text-gray-700 pb-2">{project.title}</p>
                                 <p>{project.description}</p>
--- pages/index.js
+++ pages/index.js
@@ -22,14 +22,14 @@ export default function Index() {
                 <div className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-96">
                     <svg className="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg"
                          preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
-                        <polygon className="text-gray-300 dark:text-gray-900 fill-current" points="2560 0 2560 100 0 100"></polygon>
+                        <polygon className="text-gray-300 dark:text-gray-900 fill-current" points="2560 0 2560 100 0 100"/>
                     </svg>
                 </div>
             </section>
             <section className="relative py-16">
                 <div className="container mx-auto px-4">
                     <div
-                        className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64 dark:bg-gray-800">
+                        className="bg-white w-full mb-6 shadow-xl rounded-lg -mt-64 dark:bg-gray-800">
                         <div className="px-6">
                             <div className="flex flex-wrap justify-center">
                                 <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
@@ -94,16 +94,19 @@ export default function Index() {
                                     </div>
                                 </div>
                             </div>
-                            <div className="mt-10 py-10 border-t border-gray-300 dark:border-gray-700 text-center">
-                                <div className="text-center">
-                                    <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-700 dark:text-gray-200 mb-2">
-                                        Portfolio
-                                    </h3>
-                                </div>
-                                <div className="flex flex-wrap justify-center">
-                                    <div className="w-full lg:w-10/12 px-4">
-                                        <Timeline/>
-                                    </div>
+                        </div>
+                    </div>
+                    <div
+                        className="bg-white w-full mb-6 shadow-xl rounded-lg dark:bg-gray-800">
+                        <div className="mt-10 py-10 border-t border-gray-300 dark:border-gray-700 text-center">
+                            <div className="text-center">
+                                <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-700 dark:text-gray-200 mb-2">
+                                    Portfolio
+                                </h3>
+                            </div>
+                            <div className="flex flex-wrap justify-center">
+                                <div className="w-full lg:w-10/12 px-4">
+                                    <Timeline/>
                                 </div>
                             </div>
                         </div>
