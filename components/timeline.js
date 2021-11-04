36e6ae3bd7c8ab58b069b9221c2b95b3975ff96a
Rowan Fuchs <rfuchs@999games.nl>
2021-11-04 15:28:01 +0100

    changes

--- components/timeline.js
+++ components/timeline.js
@@ -5,20 +5,18 @@ import Portfolio from '../public/portfolio';
 const Timeline = () => {
 
     return (
-
-
         <div className="timeline mt-6">
             {Portfolio && Portfolio.map((project, index) => (
                 <div key={index} className={index % 2 === 0 ? "relative mt-8" : "relative mt-20 flex flex-row-reverse"}>
-                    <span className={index % 2 === 0 ? "timeline-before" : "timeline-before timeline-before-right"}/>
+                    <span className={index % 2 === 0 ? "timeline-before" : "timeline-before left-0"}/>
                     <div className={index % 2 === 0 ? "timeline-post-marker" : "timeline-post-marker -left-2"}/>
                     <div
                         className={(project.cancelled ? "border-l-4 border-red-400" : "") + " bg-white w-10/12 float-left shadow-md rounded-lg px-6 py-4"}>
                     <span
-                        className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-purple-100 bg-purple-400 rounded">
+                        className=" py-1 text-xs font-bold leading-none text-gray-300">
                         {project.date}
                     </span>
-                        <p className="mb-3 text-l font-semibold tracking-tight text-gray-800">
+                        <p className="mb-3 text-l font-semibold tracking-tight text-gray-600">
                             {project.title}
                         </p>
                         <p className="leading-normal text-sm text-gray-600">
