01e7bc2b30c1db9dce8ba92e7f52abc739456e01
Rowan <rowanfuchs@gmail.com>
2021-11-09 18:58:22 +0100

    changes

--- components/navbar.js
+++ components/navbar.js
@@ -7,6 +7,10 @@ const Navbar = (state) => {
     return (
         <div className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between">
             <div className="container p-4 mx-auto flex flex-wrap items-center justify-between">
+                <div className="text-white text-xs block">
+                    Built with <span className="text-red-700">&nbsp;♥&nbsp;</span> on Next with React and Tailwind CSS
+                    <a className="block" href="https://vue.rowanfuchs.nl/">Switch to Vue</a>
+                </div>
                 <div className="flex flex-grow justify-end">
                     <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
                         <li className="flex items-center">
